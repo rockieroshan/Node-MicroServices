46f67624848cb3d61eb9200b18e096929af6c218
Rakesh Chandrasheker <rockieroshan@gmail.com>
2020-02-16 12:30:37 +0530

    Server.js
    Add Server.js

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.config.env' });
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening to port ${PORT} running on ${process.env.NODE_ENV}`);
+});
